2ee213a802138819d177587d40a2d37dfb4ad6ac
martynalesniak <125890163+martynalesniak@users.noreply.github.com>
2026-04-16 10:23:38 +0200

    Fix smoothie order submission and remove test code

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -36,6 +38,4 @@ if ingredients_list:
 
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
